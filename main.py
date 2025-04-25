4eef79a34872450d34523ab9d49cecec53823ee5
OofySimpsonV3 <117018176+OofySimpsonV3@users.noreply.github.com>
2025-04-25 18:12:38 +1000

    Create main.py
    Copied from the original file "yt.py", simply located within my home folder :)

---
+++ main.py
@@ -0,0 +1,144 @@
+# S5T - Super Simple Song Search (and) Sampling Tool
+# By OofySimpsonV3 (github)
+# Enjoy :)
+
+# MODULES
+from ytmusicapi import YTMusic
+from yt_dlp import YoutubeDL
+import sys
+from pprint import pprint
+
+# FETCH SONG FUNCTION
+def fetchSong(query):
+    search_results = ytmusic.search(query, filter="songs")
+
+    url = search_results[0]["videoId"]
+
+    opts = {
+        "outtmpl": "~/Music/%(title)s-%(id)s.%(ext)s",
+        "embed-thumbnail": True,
+        "format": "bestaudio/best",
+        "writethumbnail": True,
+        "postprocessors": [
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "mp3",
+                "preferredquality": "320",
+            },
+            {"key": "FFmpegMetadata"},
+            {
+                "key": "EmbedThumbnail",
+                "already_have_thumbnail": False,
+            },
+        ],
+        "addmetadata": True,
+        "add-metadata": True,
+    }
+
+    with YoutubeDL(opts) as ydl:
+        ydl.download(url)
+
+# FETCH ALBUM FUNCTION
+def fetchAlbum(query):
+    search_results = ytmusic.search(query, filter="albums")
+
+    pprint(search_results)
+
+    url = "https://music.youtube.com/playlist?list=" + search_results[0]["playlistId"]
+
+    opts = {
+        "outtmpl": "~/Music/%(playlist_title)s/%(playlist_index)s - %(title)s.%(ext)s",
+        "embed-thumbnail": True,
+        "format": "bestaudio/best",
+        "writethumbnail": True,
+        "postprocessors": [
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "mp3",
+                "preferredquality": "320",
+            },
+            {"key": "FFmpegMetadata"},
+            {
+                "key": "EmbedThumbnail",
+                "already_have_thumbnail": False,
+            },
+        ],
+        "parse-metadata": 'playlist_index:%(track_number)s',
+        "add-metadata": True,
+        "embed-metadata": True,
+    }
+
+    with YoutubeDL(opts) as ydl:
+        ydl.download(url)
+
+# FETCH YOUTUBE URL
+def fetchUrl(url):
+    opts = {
+        "outtmpl": "~/Music/%(title)s.%(ext)s",
+        "embed-thumbnail": True,
+        "format": "bestaudio/best",
+        "writethumbnail": True,
+        "postprocessors": [
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "mp3",
+                "preferredquality": "320",
+            },
+            {"key": "FFmpegMetadata"},
+            {
+                "key": "EmbedThumbnail",
+                "already_have_thumbnail": False,
+            },
+        ],
+        "addmetadata": True,
+        "add-metadata": True,
+    }
+
+    with YoutubeDL(opts) as ydl:
+        ydl.download(url) 
+
+# STARTUP
+if __name__ == "__main__":
+    usage = """
+Arguments:
+
+--url <URL>
+Downloads Youtube media via a URL
+
+--song <QUERY>
+Searches and downloads the first song result in Youtube Music.
+
+--album <QUERY>
+Searches and downloads the first album result in Youtube Music.
+
+--help
+Shows a list of commands.
+"""
+    
+    try:
+        arg1 = str(sys.argv[1])
+    except:
+        print(usage) 
+        sys.exit()
+
+    try:
+        arg2 = str(sys.argv[2])
+    except:
+        pass
+
+    try:
+        ytmusic = YTMusic()
+        
+        if arg1 == "--album":
+            fetchAlbum(arg2)
+        elif arg1 == "--song":
+            fetchSong(arg2)
+        elif arg1 == "--url":
+            fetchUrl(arg2)
+        elif arg1 == "--help":
+            print(usage)
+        else:
+            print("invalid command! Append '--help' for a list of commands.") 
+            
+    except Exception as e:
+        print(e)
